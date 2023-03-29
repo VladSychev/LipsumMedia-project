4a630e3bfb5a8ba4a85bc89c6f6b765a71450e1f
Владислав <104278445+VladSychev@users.noreply.github.com>
2023-03-29 20:48:05 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+/* Импортируем функцию работы слайдера */
+import { sliderFunction } from "./modules/slider/index.js";
+/* Импортируем функцию работы описания слайдов */
+import { sliderDiscriptionFunction } from "./modules/sliderDiscription/sliderDiscription.js";
